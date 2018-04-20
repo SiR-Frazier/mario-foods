502a9cd8149555362bae202641edb3bde5259d2a
Bryce Frazier <bx.frazier@gmail.com>
2018-04-20 10:32:49 -0700

    add form for new product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,14 @@ class ProductsController < ApplicationController
     render :show
   end
 
+  def new
+    @product = Product.new
+    render :new
+  end
 
-    
+
+  private
+    def product_params
+      params.require(:product).permit(:name)
+    end  
 end
