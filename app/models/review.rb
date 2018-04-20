0468ebe52da4cfb1d38ed1f71d1e262dd9d70bb2
Bryce Frazier <bx.frazier@gmail.com>
2018-04-20 14:12:56 -0700

    review is visible on the product page

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
-  belongs_to :products
+  belongs_to :product
   validates :author, :presence => true
 
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Review do
   it { should validate_presence_of :author }
+  it { should belong_to :product } ##May need to change to products here and in review.rb
 end
