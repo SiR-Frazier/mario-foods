78c0077f6089be850eabb5f16367d010e0ee286a
Bryce Frazier <bx.frazier@gmail.com>
2018-04-20 16:57:34 -0700

    able to seed products, but not reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.destroy_all
+Review.destroy_all
 
 50.times do |index|
   Product.create!(name: Faker::Food.ingredient,
                           country_origin: Faker::Space.galaxy,
                           price: Faker::Number.between(1,35))
-  250.times do |index|
-    Review.create!(author: Faker::Name.first_name,
-                            rating: Faker::Number.between(1,5),
-                            comment: Faker::Simpsons.quote)
-  end
+  # 250.times do |index|
+  #   Review.create!(author: Faker::Name.first_name,
+  #                           rating: Faker::Number.between(1,5),
+  #                           comment: Faker::Simpsons.quote,
+  #                           product_id: Faker::Number.between(1,250))
+  # end
 end
 
 p "Created #{Product.count} products"
+p "Created #{Review.count} reviews"
