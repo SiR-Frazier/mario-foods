3015b31eba742113cb528f5672b55da4a3a592f9
Bryce Frazier <bx.frazier@gmail.com>
2018-04-20 14:44:12 -0700

    new review page is rendering. Removed the unecessary content from the layout/_error.html.erb file

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ActiveRecord::Base
-  belongs_to :product
+  belongs_to :product #if I run into issues, double-check that it's not products
   validates :author, :presence => true
 
 end
