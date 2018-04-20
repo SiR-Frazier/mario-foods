5446ae64e9f64ea989eb154acfa13d28d9d423fd
Bryce Frazier <bx.frazier@gmail.com>
2018-04-20 11:01:58 -0700

    add styling to index and form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,9 +14,17 @@ class ProductsController < ApplicationController
     render :new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end    
 
   private
     def product_params
       params.require(:product).permit(:name)
-    end  
+    end
 end
