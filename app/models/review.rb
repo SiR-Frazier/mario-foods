272c116d8d41f57ca8fad6522e5e30c53c22b54f
sir-frazier <bxfrazier@gmail.com>
2018-04-25 21:36:13 -0700

    update validations for review and product in rb files

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :product #if I run into issues, double-check that it's not products
   validates :author, :presence => true
+  validates_numericality_of :rating, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
+  validates :comment, presence => true, length: { in: 50..250 }
+  #validates :product_id presence => true ## may not be necessary
 
 end
