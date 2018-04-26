32b84eb648eeaf275fbf9e58b20e1a9e245e4461
sir-frazier <bxfrazier@gmail.com>
2018-04-25 21:07:03 -0700

    added root route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Product.destroy_all
 Review.destroy_all
 
@@ -13,12 +5,12 @@ Review.destroy_all
   Product.create!(name: Faker::Food.ingredient,
                           country_origin: Faker::Space.galaxy,
                           price: Faker::Number.between(1,35))
-  # 250.times do |index|
-  #   Review.create!(author: Faker::Name.first_name,
-  #                           rating: Faker::Number.between(1,5),
-  #                           comment: Faker::Simpsons.quote,
-  #                           product_id: Faker::Number.between(1,250))
-  # end
+  250.times do |index|
+    Review.create!(author: Faker::Name.first_name,
+                            rating: Faker::Number.between(1,5),
+                            comment: Faker::Simpsons.quote,
+                            product_id: Faker::Number.between(1,250))
+  end
 end
 
 p "Created #{Product.count} products"
