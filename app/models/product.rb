f2bc533d1bccd130f54f8c3d5faeac3bf6e99487
sir-frazier <bxfrazier@gmail.com>
2018-04-28 14:32:23 -0700

    update length validations for review

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ActiveRecord::Base
   has_many :reviews
   validates :name, :presence => true
   validates :country_origin, :presence => true
-  validates :price, 
+  validates :price, :presence => true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :product #if I run into issues, double-check that it's not products
   validates :author, :presence => true
   validates_numericality_of :rating, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
-  validates :comment, presence => true, length: { in: 50..250 }
+  validates_length_of :comment, minimum: 50, maximum: 250, presence => true
   #validates :product_id presence => true ## may not be necessary
 
 end
