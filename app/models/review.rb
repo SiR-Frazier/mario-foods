39f38bcc58372435867e217dc46d216802d4aec6
sir-frazier <bxfrazier@gmail.com>
2018-04-28 14:39:22 -0700

    update rating validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :product #if I run into issues, double-check that it's not products
   validates :author, :presence => true
-  validates_numericality_of :rating, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
+  validates_inclusion_of :rating, :in => 1..5
   validates_length_of :comment, minimum: 50, maximum: 250, presence => true
   #validates :product_id presence => true ## may not be necessary
 
