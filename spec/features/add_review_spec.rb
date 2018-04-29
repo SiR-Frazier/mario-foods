1a724eb4df0446a3ab104ba0622766efc02ca254
sir-frazier <bxfrazier@gmail.com>
2018-04-29 15:25:27 -0700

    add review spec. not recognizing product id

---
+++ spec/features/add_review_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+# describe "create a new review" do
+#   it "adds a new review" do
+#     visit new_product_review_path(product)
+#     click_link 'Leave a review'
+#     fill_in 'comment', :with => 'amazing! I love eating it with salad and salmon. I cannot recommend it enough!'
+#     fill_in 'rating', :with => '5'
+#     fill_in 'author', :with => 'Ryan'
+#     click_on 'Create Review'
+#     expect(page).to have_content '/products/:id'
+#   end
+# end
