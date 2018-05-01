873ed8226bdcbc089cb8181119c39830d5ae71a8
sir-frazier <bxfrazier@gmail.com>
2018-04-30 20:39:42 -0700

    add additional validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe Product do
   it { should validate_presence_of :name }
   it { should have_many :reviews }
+  it { should validate_presence_of :country_origin }
+  it { should validate_presence_of :price }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 describe Review do
   it { should validate_presence_of :author }
-  it { should belong_to :product } ##May need to change to products here and in review.rb
+  it { should belong_to :product }
+  it { validate_inclusion_of :rating }
+  it { validate_length_of :comment }
 end
